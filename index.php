4fd0a2f3c4ad1c686333b4c83263a35deea1eac3
Administrator <admin@example.com>
2019-07-16 18:42:09 +0800

    5 delay

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
  <body>
 <?php
 // current time
-echo "<p>"date('h:i:s')"</p>" . "\n";
+echo "<p>" . date('h:i:s') . "</p>" . "\n";
 // sleep for 10 seconds
 $delay_sec = getenv('DELAY_SEC');
 // sleep(delay_sec);
 sleep(10);
 // wake up !
-echo "<p>"date('h:i:s')"</p>" . "\n";
+echo "<p>" . date('h:i:s') . "</p>" . "\n";
 ?>
  </body>
 </html>
