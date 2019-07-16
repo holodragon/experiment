9d97c0dc0f321a12c01bf10a0ebd97cc802e4c7b
Administrator <admin@example.com>
2019-07-16 18:25:18 +0800

    10 delay

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+   <title>Hello World</title>
+ </head>
+ <body>
+<?php
+// current time
+echo date('h:i:s') . "\n";
+// sleep for 10 seconds
+// sleep(900);
+sleep(10);
+// wake up !
+echo date('h:i:s') . "\n";
+?>
+ </body>
+</html>
